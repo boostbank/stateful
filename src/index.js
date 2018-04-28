6af63839a223d0337a06007671b7666a0aa54240
dbteku <dbteku@hotmail.com>
2018-04-27 18:10:27 -0600

    moved everything to index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,82 @@
-const stateful = require("./stateful");
+("use strict");
 
-main();
+const OBJECT = "object";
+const BOOLEAN = "boolean";
+const FUNCTION = "function";
+const NUMBER = "number";
+const STRING = "string";
+const UNDEFINED = "undefined";
 
-function main() {
-  stateful.createStore({ Test: "gay" });
-  console.log(stateful.getState());
-  stateful.modify(modifier, { fag: true });
-  console.log(stateful.getState());
+let instance = undefined;
+
+let currentStore = undefined;
+
+const states = [];
+
+const getInstance = () => {
+  if (instance === undefined) {
+    instance = new Stateful();
+  }
+  return instance;
+};
+
+const pushToStack = newState => {
+  if (getInstance().maxDepth >= 0) {
+    if (states.length < getInstance().maxDepth) {
+      states.push(newState);
+    }
+  } else {
+    states.push(newState);
+  }
+};
+
+/**
+ * @module Stateful
+ */
+class Stateful {
+  constructor() {
+    this.maxDepth = -1;
+  }
+
+  createStore(store = {}, maxDepth = -1) {
+    currentStore = { ...store };
+    pushToStack(currentStore);
+    getInstance().maxDepth = maxDepth;
+  }
+
+  rollback() {
+    if (states.length >= 1) {
+      states.splice(states.length - 1, 1);
+      currentStore = states[states.length - 1];
+    }
+  }
+
+  modify(modifier, payload) {
+    if (modifier && typeof modifier === FUNCTION) {
+      const newState = modifier(getInstance().getState(), payload);
+      if (
+        newState !== currentStore &&
+        newState !== undefined &&
+        newState !== null &&
+        typeof newState === OBJECT
+      ) {
+        currentStore = { ...newState };
+        pushToStack(currentStore);
+      }
+    }
+  }
+
+  clear() {
+    currentStore = {};
+    states = [];
+    pushToStack(currentStore);
+  }
+
+  getState() {
+    return { ...currentStore };
+  }
 }
 
-function modifier(currentState, payload) {
-  delete currentState.Test;
-  currentState.fag = payload.fag;
-  return currentState;
-}
+var initializer = getInstance();
+
+module.exports = initializer;
