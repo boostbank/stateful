e13fc81e0595aa28e70f7c9fc64c53542e68c830
dbteku <dbteku@hotmail.com>
2018-04-27 18:05:13 -0600

    No need for selector.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const stateful = require("./stateful");
 
+main();
+
 function main() {
-  stateful.createStore({Test: "gay"});
+  stateful.createStore({ Test: "gay" });
+  console.log(stateful.getState());
+  stateful.modify(modifier, { fag: true });
   console.log(stateful.getState());
 }
 
-main();
+function modifier(currentState, payload) {
+  delete currentState.Test;
+  currentState.fag = payload.fag;
+  return currentState;
+}
--- src/stateful.js
+++ src/stateful.js
@@ -1,4 +1,13 @@
-"use strict";
+import { cursorTo } from "readline";
+
+("use strict");
+
+const OBJECT = "object";
+const BOOLEAN = "boolean";
+const FUNCTION = "function";
+const NUMBER = "number";
+const STRING = "string";
+const UNDEFINED = "undefined";
 
 let instance = undefined;
 
@@ -13,24 +22,61 @@ const getInstance = () => {
   return instance;
 };
 
+const pushToStack = newState => {
+  if (getInstance().maxDepth >= 0) {
+    if (states.length < getInstance().maxDepth) {
+      states.push(newState);
+    }
+  } else {
+    states.push(newState);
+  }
+};
+
 /**
  * @module Stateful
  */
 class Stateful {
   constructor() {
-      this.maxDepth = -1;
+    this.maxDepth = -1;
   }
 
-  createStore(store = {}, maxDepth = -1){
-    currentStore = store;
-    states.push(currentStore)
+  createStore(store = {}, maxDepth = -1) {
+    currentStore = { ...store };
+    pushToStack(currentStore);
     getInstance().maxDepth = maxDepth;
   }
 
-  getState(){
-      return {...currentStore};
+  rollback() {
+    if (states.length >= 1) {
+      states.splice(states.length - 1, 1);
+      currentStore = states[states.length - 1];
+    }
   }
 
+  modify(modifier, payload) {
+    if (modifier && typeof modifier === FUNCTION) {
+      const newState = modifier(getInstance().getState(), payload);
+      if (
+        newState !== currentStore &&
+        newState !== undefined &&
+        newState !== null &&
+        typeof newState === OBJECT
+      ) {
+        currentStore = { ...newState };
+        pushToStack(currentStore);
+      }
+    }
+  }
+
+  clear() {
+    currentStore = {};
+    states = [];
+    pushToStack(currentStore);
+  }
+
+  getState() {
+    return { ...currentStore };
+  }
 }
 
 var initializer = getInstance();
