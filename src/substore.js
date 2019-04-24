d8870a62459bbf3c0533cfe7378649fb2d9f9bfa
dbteku <dbteku@hotmail.com>
2019-04-24 13:33:18 -0600

    No more regex. their identifiers can be whatever they want.

--- src/substore.js
+++ src/substore.js
@@ -15,25 +15,15 @@ const subStores = {};
 
 const lookup = {};
 
-const checkForNumber = uid => {
-  if (uid.length >= 1) {
-    if (!isNaN(uid.charAt(0))) {
-      throw new Error("First character cannot start with number!");
-    }
-  }
-};
-
 const isValid = uid => {
-  return !/[~`!#$%\^&*+=\[\]\\';,/{}|\\":<>\?]/g.test(uid);
+  return typeof uid === "string" && uid.length > 0;
 };
 
 class SubStore {
   createSubStore(uid = undefined, store = {}, maxDepth = -1) {
     let subStore = undefined;
-    if (typeof uid === "string" && uid.length > 0) {
-      checkForNumber(uid);
       if (!isValid(uid)) {
-        throw new Error("UID cannot contain special characters!");
+        throw new Error("UID must be a string with at least 1 character.");
       }
       if (subStores.hasOwnProperty(uid)) {
         throw new Error("That store already exists!");
@@ -42,9 +32,6 @@ class SubStore {
       subStore = stateful.createStore(store, maxDepth);
       lookup[uid] = uid;
       subStores[uid] = subStore;
-    } else {
-      throw new Error("You must pass a unique identifier!");
-    }
     return subStore;
   }
 
