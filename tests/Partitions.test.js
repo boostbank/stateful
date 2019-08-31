3333f2d982480ed2407a8e45e35223b5954b5d0c
dbteku <dbteku@hotmail.com>
2019-08-31 01:41:25 -0600

    Substore tests

--- tests/Partitions.test.js
+++ tests/Partitions.test.js
@@ -41,6 +41,53 @@ describe("Partition Tests", () => {
 
     expect(storeCreate.created).toBe(true);
     expect(storeCreate.store).not.toBe(null);
+  });
 
+  it("Doesn't create duplicate store.", () => {
+    const created = Partitions.createPartition("test");
+    expect(created).toBe(true);
+    expect(Partitions.get("test")).not.toBe(null);
+
+    const storeCreate = Partitions.createStore("test");
+    expect(storeCreate.created).toBe(true);
+    expect(storeCreate.store).not.toBe(null);
+
+    expect(() => {
+      Partitions.createStore("test");
+    }).toThrowError();
+  });
+
+  it("Creates a subStore.", () => {
+    const created = Partitions.createPartition("test");
+    expect(created).toBe(true);
+    expect(Partitions.get("test")).not.toBe(null);
+
+    const storeCreate = Partitions.createSubStore("test", "test");
+
+    expect(storeCreate.created).toBe(true);
+    expect(storeCreate.store).not.toBe(null);
+  });
+
+  it("Doesn't create duplicate subStore.", () => {
+    const created = Partitions.createPartition("test");
+    expect(created).toBe(true);
+    expect(Partitions.get("test")).not.toBe(null);
+
+    const storeCreate = Partitions.createSubStore("test", "test");
+    expect(storeCreate.created).toBe(true);
+    expect(storeCreate.store).not.toBe(null);
+
+    expect(() => {
+      const storeCreate = Partitions.createSubStore("test", "test");
+    }).toThrowError();
+  });
+
+  it("Doesn't create a subStore with no uid.", () => {
+    const created = Partitions.createPartition("test");
+    expect(created).toBe(true);
+    expect(Partitions.get("test")).not.toBe(null);
+    expect(() => {
+      Partitions.createSubStore("test");
+    }).toThrowError();
   });
 });
