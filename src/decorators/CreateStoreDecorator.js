19c9e828d72336d2177e8612d3bd143f387d7965
dbteku <dbteku@hotmail.com>
2019-08-31 12:54:09 -0600

    Adding docs

--- src/decorators/CreateStoreDecorator.js
+++ src/decorators/CreateStoreDecorator.js
@@ -3,11 +3,21 @@ const Partitions = require("../utils/Partitions");
 class CreateStoreDecorator {
   constructor() {}
 
-
+/**
+ * Create a store on a partition.
+ * @param {String} id 
+ * @param {Object} store 
+ * @param {Number} depth 
+ */
   onPartition(id = "", store = {}, depth = 0) {
     return Partitions.createStore(id, store, depth);
   }
 
+  /**
+   * Create a shared global store.
+   * @param {Object} store 
+   * @param {Number} depth 
+   */
   global(store = {}, depth = 0) {
     const id = Partitions.getDefaultScope();
     return Partitions.createStore(id, store, depth);
